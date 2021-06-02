7da97f0551fbd927c1df63b1c76a3dd79964d197
carushi <rkawaguc@cshl.edu>
2021-06-02 14:53:38 -0400

    edit tutorials

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='Catactor',
-    version='0.1.1',
+    version='0.1.2',
     description="scATAC-seq analysis using meta-analytic marker genes",
     long_description=long_description,
     long_description_content_type="text/markdown",
